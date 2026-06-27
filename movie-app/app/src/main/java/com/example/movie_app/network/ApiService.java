216180a9c9f9123e5052f9f12b51eff9a1075afd
NguyenNhatHung <23130129@st.hcmuaf.edu.vn>
2026-06-27 14:44:25 +0700

    fix(admin): bỏ api login

--- movie-app/app/src/main/java/com/example/movie_app/network/ApiService.java
+++ movie-app/app/src/main/java/com/example/movie_app/network/ApiService.java
@@ -52,12 +52,6 @@ public interface ApiService {
     @DELETE("/api/v1/resume/{movieId}")
     Call<Void> deleteResumeData(@Path("movieId") String movieId);
 
-    @POST("api/v1/auth/login")
-    Call<LoginResponse> loginUser(@Body LoginRequest request);
-
-    @POST("api/v1/auth/register")
-    Call<RegisterResponse> registerUser(@Body RegisterRequest request);
-
     @GET("api/v1/admin/stats/summary")
     Call<AdminStatsResponse> getAdminStats();
 
